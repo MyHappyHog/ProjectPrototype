8cc985f063ce8dc7581e73121933aabf34e0676a
gyu-hyeon <ngh3053@gmail.com>
2015-09-09 02:04:33 +0900

    simple communication
    when connection is successed, client send a message to server.

--- src/02.Android/networkTest/app/src/main/java/com/example/ngh1/networktest/MainActivity.java
+++ src/02.Android/networkTest/app/src/main/java/com/example/ngh1/networktest/MainActivity.java
@@ -1,105 +1,372 @@
 package com.example.ngh1.networktest;
 
 import android.app.Activity;
-import android.os.AsyncTask;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.format.Formatter;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 public class MainActivity extends Activity {
+    private EditText et1, et2, et3;
+    private TextView tv4;
+    private Socket socket;
+    private DataOutputStream writeSocket;
+    private DataInputStream readSocket;
+    private Handler mHandler = new Handler();
 
-    private TextView txtResponse;
-    private EditText edtTextAddress, edtTextPort, edtTextMystr;
-    private Button btnConnect, btnClear;
+    private ConnectivityManager cManager;
+    private NetworkInfo wifi;
+    private ServerSocket serverSocket;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        edtTextAddress = (EditText) findViewById(R.id.address);
-        edtTextPort = (EditText) findViewById(R.id.port);
-        edtTextMystr = (EditText) findViewById(R.id.mystr);
-        btnConnect = (Button) findViewById(R.id.connect);
-        btnClear = (Button) findViewById(R.id.clear);
-        txtResponse = (TextView) findViewById(R.id.response);
+        et1 = (EditText) findViewById(R.id.editText1);
+        et2 = (EditText) findViewById(R.id.editText2);
+        et3 = (EditText) findViewById(R.id.editText3);
 
-        btnConnect.setOnClickListener(buttonConnectOnClickListener);
-        btnClear.setOnClickListener(new OnClickListener() {
+        tv4 = (TextView) findViewById(R.id.textView4);
 
-            public void onClick(View v) {
-                txtResponse.setText("");
-            }
-        });
+        cManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
     }
 
-    // 클릭이벤트 리스너
-    OnClickListener buttonConnectOnClickListener = new OnClickListener() {
-
-        public void onClick(View arg0) {
-            NetworkTask myClientTask = new NetworkTask(
-                    edtTextAddress.getText().toString(),
-                    Integer.parseInt(edtTextPort.getText().toString()),
-                    edtTextMystr.getText().toString()
-            );
-            myClientTask.execute();
+    @SuppressWarnings("deprecation")
+    public void OnClick(View v) throws Exception {
+        switch (v.getId()) {
+            case R.id.button1:
+                (new Connect()).start();
+                break;
+            case R.id.button2:
+                (new Disconnect()).start();
+                break;
+            case R.id.button3:
+                (new SetServer()).start();
+                break;
+            case R.id.button4:
+                (new CloseServer()).start();
+                break;
+            case R.id.button5:
+                wifi = cManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+                if (wifi.isConnected()) {
+                    WifiManager wManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+                    WifiInfo info = wManager.getConnectionInfo();
+                    tv4.setText("IP Address : " + Formatter.formatIpAddress(info.getIpAddress()));
+                } else {
+                    tv4.setText("Disconnected");
+                }
+                break;
+            case R.id.button6:
+                (new sendMessage()).start();
         }
-    };
+    }
 
-    public class NetworkTask extends AsyncTask<Void, Void, Void> {
-
-        String dstAddress;
-        int dstPort;
-        String dstMystr;
-        String response;
-
-        NetworkTask(String addr, int port, String mystr) {
-            dstAddress = addr;
-            dstPort = port;
-            dstMystr = mystr;
-        }
-
-        @Override
-        protected Void doInBackground(Void... arg0) {
+    class Connect extends Thread {
+        public void run() {
+            Log.d("Connect", "Run Connect");
+            String ip = null;
+            int port = 0;
 
             try {
-                Socket socket = new Socket(dstAddress, dstPort);
-                InputStream inputStream = socket.getInputStream();
-                ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
-                        1024);
-                byte[] buffer = new byte[1024];
+                ip = et1.getText().toString();
+                port = Integer.parseInt(et2.getText().toString());
+            } catch (Exception e) {
+                final String recvInput = "정확히 입력하세요!";
+                mHandler.post(new Runnable() {
 
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    byteArrayOutputStream.write(buffer, 0, bytesRead);
-                }
-
-                socket.close();
-                response = byteArrayOutputStream.toString("UTF-8");
-
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+                });
             }
-            return null;
-        }
+            try {
+                socket = new Socket(ip, port);
+                writeSocket = new DataOutputStream(socket.getOutputStream());
+                readSocket = new DataInputStream(socket.getInputStream());
 
-        @Override
-        protected void onPostExecute(Void result) {
-            txtResponse.setText(response);
-            super.onPostExecute(result);
-        }
+                mHandler.post(new Runnable() {
 
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast("연결에 성공하였습니다.");
+                    }
+
+                });
+                (new recvSocket()).start();
+            } catch (Exception e) {
+                final String recvInput = "연결에 실패하였습니다.";
+                Log.d("Connect", e.getMessage());
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+
+                });
+
+            }
+
+        }
     }
 
+    class Disconnect extends Thread {
+        public void run() {
+            try {
+                if (socket != null) {
+                    socket.close();
+                    mHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            setToast("연결이 종료되었습니다.");
+                        }
+                    });
+
+                }
+
+            } catch (Exception e) {
+                final String recvInput = "연결에 실패하였습니다.";
+                Log.d("Connect", e.getMessage());
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+
+                });
+
+            }
+
+        }
+    }
+
+    class SetServer extends Thread {
+
+        public void run() {
+            try {
+                int port = Integer.parseInt(et2.getText().toString());
+                serverSocket = new ServerSocket(port);
+                final String result = "서버 포트 " + port + " 가 준비되었습니다.";
+
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(result);
+                    }
+                });
+
+                socket = serverSocket.accept();
+                writeSocket = new DataOutputStream(socket.getOutputStream());
+                readSocket = new DataInputStream(socket.getInputStream());
+
+                while (true) {
+                    byte[] b = new byte[100];
+                    int ac = readSocket.read(b, 0, b.length);
+                    String input = new String(b, 0, b.length);
+                    final String recvInput = input.trim();
+
+                    if(ac==-1)
+                        break;
+
+                    mHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            setToast(recvInput);
+                        }
+
+                    });
+                }
+                mHandler.post(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast("연결이 종료되었습니다.");
+                    }
+
+                });
+                serverSocket.close();
+                socket.close();
+            } catch (Exception e) {
+                final String recvInput = "서버 준비에 실패하였습니다.";
+                Log.d("SetServer", e.getMessage());
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+
+                });
+
+            }
+
+        }
+    }
+
+    class recvSocket extends Thread {
+
+        public void run() {
+            try {
+                readSocket = new DataInputStream(socket.getInputStream());
+
+                while (true) {
+                    byte[] b = new byte[100];
+                    int ac = readSocket.read(b, 0, b.length);
+                    String input = new String(b, 0, b.length);
+                    final String recvInput = input.trim();
+
+                    if(ac==-1)
+                        break;
+
+                    mHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            setToast(recvInput);
+                        }
+
+                    });
+                }
+                mHandler.post(new Runnable(){
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast("연결이 종료되었습니다.");
+                    }
+
+                });
+            } catch (Exception e) {
+                final String recvInput = "연결에 문제가 발생하여 종료되었습니다..";
+                Log.d("SetServer", e.getMessage());
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+
+                });
+
+            }
+
+        }
+    }
+
+    class CloseServer extends Thread {
+        public void run() {
+            try {
+                if (serverSocket != null) {
+                    serverSocket.close();
+                    socket.close();
+
+                    mHandler.post(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            setToast("서버가 종료되었습니다..");
+                        }
+                    });
+                }
+            } catch (Exception e) {
+                final String recvInput = "서버 준비에 실패하였습니다.";
+                Log.d("SetServer", e.getMessage());
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+
+                });
+
+            }
+
+        }
+    }
+
+    class sendMessage extends Thread {
+        public void run() {
+            try {
+                byte[] b = new byte[100];
+                b = et3.getText().toString().getBytes();
+                writeSocket.write(b);
+
+            } catch (Exception e) {
+                final String recvInput = "메시지 전송에 실패하였습니다.";
+                Log.d("SetServer", e.getMessage());
+                mHandler.post(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        setToast(recvInput);
+                    }
+
+                });
+
+            }
+
+        }
+    }
+
+    void setToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
