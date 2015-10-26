e51ccafba380bf1b83f5cf3544f8ce9e7fd4a455
Yoonseung Choi <cheh344@gmail.com>
2015-10-26 21:23:14 +0900

    Current Sensor for AC - Refactoring
    comment added

--- src/01.HW/CurrentSensor/CurrentSensorAC/CurrentSensorAC.ino
+++ src/01.HW/CurrentSensor/CurrentSensorAC/CurrentSensorAC.ino
@@ -1,9 +1,8 @@
-int pin = A0;
+int pin = A0; 
 double value = 0;
  
 int rmsArray[50];
 double result;
-int N = 50;
 int counter = 0;
  
 void setup(){
@@ -11,21 +10,39 @@ void setup(){
 }
  
 void loop(){
-  value = analogRead(pin);
-  if(counter++ < N){
-    rmsArray[counter] = value - 345.5;
-    delay(1);
-  }
-  else{
-    counter = 0;
-    result = 0;
-    for(int i = 0; i < N; i++){
-      result += pow(rmsArray[i], 2);
-    }
-    result /= N;
-    result = sqrt(result);
-    delay(500);
-    Serial.print("A0 = ");
-    Serial.println(result);
-  }
+  
+  value = analogRead(pin); // get raw value of 'Vout'
+  
+  if(counter++ < 50) // number of cumulated data is less than 50
+    {cumulateValue();} // cumulate data 
+  
+  else 
+    {getRMSResult();} // calculate AC Current data
+    
+  delay(1000);
+}
+
+void cumulateValue() {
+  rmsArray[counter] = value;
+  delay(1);
+}
+
+void getRMSResult() {
+
+  // reset some values
+  counter = 0;
+  result = 0;
+  
+  // AC Current is priodically reversed
+  // power data to eliminate minus
+  for(int i = 0; i < N; i++) 
+    result += pow(rmsArray[i], 2);
+  
+  // get average value
+  result /= N;
+  result = sqrt(result);
+   
+  // print average of AC Current data  
+  Serial.print("A0 = ");
+  Serial.println(result);
 }
