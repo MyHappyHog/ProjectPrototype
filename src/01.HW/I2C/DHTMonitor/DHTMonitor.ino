de6c5b7ffdb93b325239856a32d3c571633a4c28
Yoonseung Choi <cheh344@gmail.com>
2015-11-12 08:23:39 +0900

    [I2C] DHT Monitor
    DHT Monitor for Arduino pro mini
    * Must verify configuration before upload sketch

--- src/01.HW/I2C/DHTMonitor/DHTMonitor.ino
+++ src/01.HW/I2C/DHTMonitor/DHTMonitor.ino
@@ -1,9 +1,14 @@
 // DHT Monitor
+
+// Processor configuration for Arduino pro mini:
+// Atmega328 (3.3v 8 Mhz)
+
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <DHT.h>
 
-#define DHTPIN 10     // what pin we're connected to
+#define DHTPIN 5     // what pin we're connected to
+
 // Uncomment whatever type you're using!
 #define DHTTYPE DHT11   // DHT 11
 
@@ -15,8 +20,9 @@ LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars
 
 void setup() {
   // initialize the lcd 
-  Serial.begin(9600);
+  Serial.begin(115200);
   lcd.init();
+  lcd.backlight();
   
   // begin the DHT
   dht.begin();
@@ -24,22 +30,20 @@ void setup() {
 }
 
 void loop() {
-  int count = 1;
-  Serial.println(dht.readTemperature());
-
   
   delay(500);
   lcd.setCursor(0,0); // first box of first line
   lcd.print("Temp:");
   
   lcd.setCursor(7,0); // eighth box of first line
-  lcd.print(dht.readTemperature());
+  lcd.print((int)dht.readTemperature());
+  lcd.print("'C");
   
   lcd.setCursor(0,1);// first box of second line
   lcd.print("Humid:");
   
   lcd.setCursor(7,1); // eighth box of first line
-  lcd.print(dht.readHumidity());
-
+  lcd.print((int)dht.readHumidity());
+  lcd.print(" %");
 
 }
