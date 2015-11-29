c884940739db4a66f41320a72f8c7b9dd5e8ed3e
kiyeonee <kiriung@naver.com>
2015-11-29 22:26:09 +0900

    upgrade function read/write password/ssid
    change function to write password/ssid length and password/ssid on eeprom

--- src/01.HW/EEPROMTest/init_eeprom/init_eeprom.ino
+++ src/01.HW/EEPROMTest/init_eeprom/init_eeprom.ino
@@ -44,25 +44,71 @@ void loop() {
   delay(1000);
 }
 
-void clear_eeprom() {
-    for ( int i = 0 ; i < EEPROM_SIZE ; i++ )
-      EEPROM.write(i, 0);
+String loadConfigure() {
+  EEPROM.begin( EEPROM_SIZE );
+  ssid = read_ssid_from_eeprom( FIRST_ADDRESS_OF_SSID );
+  password = read_password_from_eeprom( FIRST_ADDRESS_OF_PASSWORD );
+  //bootMode = EEPROM.read( IS_FIRST_BOOT );
 }
 
-String read_eeprom(int firstAddress, int length){
-  String readText = String();
+String read_ssid_from_eeprom( int firstAddress ) {
+  String read_ssid = String();
+  int ssid_length = int(EEPROM.read(firstAddress++));
   
-  for(int forI = 0; forI < length; forI++){
-    readText += char(EEPROM.read(firstAddress++));
+  for (int forLen = 0; forLen < ssid_length; forLen++) {
+    read_ssid += char(EEPROM.read(firstAddress++));
   }
-  return readText;
+  return read_ssid;
+
+  // eeprom 에서 ssid를 읽어옴.
+  // 가장 첫 주소의 값은 ssid 문장의 길이를 나타냄.
+  // 읽은 데이터를 String object로 리턴 함.
 }
 
-int write_eeprom(int address, String writeText) {
-  
-  for(int i = 0; i < writeText.length(); i++) {
-    EEPROM.write(address++, writeText.charAt(i));
+/*
+ *  EEPROM에서 ssid를 읽어들이는 함수
+ */
+void write_ssid_to_eeprom(int address, String _ssid ) {
+
+  for (int i = 0; i <= _ssid.length(); i++) {
+    if (i == 0) {
+      EEPROM.write(address++, _ssid.length());
+    }
+    else {
+      EEPROM.write(address++, _ssid.charAt(i - 1));
+    }
   }
+
   EEPROM.commit();
-  return address;
+
+  // eeprom 에다가 ssid를 씀.
+  // 가장 첫 주소의 값은 ssid 문장의 길이를 나타냄.
+  // password의 주소 부분을 침범하지 않도록 ssid가 100자가 넘는지 확인해야 함..
+  // password 주소 = 100번지 부터.. ssid는 0번지부터.. 즉 0~99로 100의 공간이 존재.
+  // but 주소의 첫 바이트는 길이이므로 99자리가 남음.
+  // 100 자리 이상일 경우에는 업데이트하지 않고 함수 종료
+}
+
+String read_password_from_eeprom( int firstAddress ) {
+  String read_password = String();
+  int ssid_length = int(EEPROM.read(firstAddress++));
+  
+  for (int forLen = 0; forLen < ssid_length; forLen++) {
+    read_password += char(EEPROM.read(firstAddress++));
+  }
+  return read_password;
+}
+
+void write_password_to_eeprom(int address, String _password ) {
+  
+  for (int i = 0; i <= _password.length(); i++) {
+    if (i == 0) {
+      EEPROM.write(address++, _password.length());
+    }
+    else {
+      EEPROM.write(address++, _password.charAt(i - 1));
+    }
+  }
+
+  EEPROM.commit();
 }
