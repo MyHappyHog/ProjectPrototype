a111d2ce654aa3ce8ce1ef9fff877027cf07d8ea
JongHo Lim <sloth@kookmin.ac.kr>
2016-02-11 01:45:50 +0900

    add SensingInfo class   - inherit Setting class   - implement deserialize and serialize

---
+++ src/01.HW/library/happyhog/SensingInfo.cpp
@@ -0,0 +1,52 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include "SensingInfo.h"
+
+SensingInfo::SensingInfo(String& fileName) : Setting(){
+	SensingInfo::SensingInfo(String("/"), fileName);
+}
+
+SensingInfo::SensingInfo(String& filePath, String& fileName) : Setting(filePath, fileName) {
+	data = new SensorData;
+	data->temperature = data->humidity = 0;
+}
+
+SensingInfo::~SensingInfo() {
+	delete data;
+}
+
+String SensingInfo::serialize() {
+	// Jsonbuffer 동적할당
+	StaticJsonBuffer<SENSORDATA_JSON_SIZE>* jsonBuffer = new StaticJsonBuffer<SENSORDATA_JSON_SIZE>;
+
+	// Senser Data를 JSON 로 변환
+	JsonObject& root = jsonBuffer->createObject();
+	root[TEMPERATURE_KEY] = data->temperature;
+	root[HUMIDITY_KEY] = data->humidity;
+
+	// JSON을 스트링으로 변환
+	String json = "";
+	root.printTo(json);
+
+	// 동적할당한 메모리 반환
+	delete jsonBuffer;
+
+	return json;
+}
+
+int SensingInfo::deserialize(String& json) {
+	// Jsonbuffer 동적할당
+	StaticJsonBuffer<SENSORDATA_JSON_SIZE>* jsonBuffer = new StaticJsonBuffer<SENSORDATA_JSON_SIZE>;
+
+	// JSON 분석
+	JsonObject& root = jsonBuffer->parseObject(json);
+	if( !root.success() ) {
+		return -1;
+	}
+
+	// 분석한 데이터 입력
+	data->temperature = root[TEMPERATURE_KEY];
+	data->humidity = root[HUMIDITY_KEY];
+
+	return 0;
+}
---
+++ src/01.HW/library/happyhog/SensingInfo.h
@@ -0,0 +1,35 @@
+#ifndef __SENSINGINFO_H__
+#define __SENSINGINFO_H__
+
+#include "Setting.h"
+
+#define TEMPERATURE_KEY "temperature"
+#define HUMIDITY_KEY "humidity"
+
+#define SENSORDATA_JSON_SIZE 200
+
+typedef struct _SensorData {
+  double temperature;
+  double humidity;
+} SensorData;
+
+/// @brief		Sensing된 데이터를 담고 있는 클래스
+/// @details	
+/// @author		Jongho Lim, sloth@kookmin.ac.kr
+/// @date		2016-02-11
+/// @version	0.0.1
+
+class SensingInfo : Setting {
+public :
+	SensingInfo(String& fileName);
+	SensingInfo(String& filePath, String& fileName);
+	~SensingInfo();
+	
+	int deserialize(String& json);
+	String serialize();
+
+private :
+	SensorData* data;
+};
+
+#endif
