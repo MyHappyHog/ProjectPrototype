0df7ffb953aaf5a22dd8464df14f917d8561ec7c
JongHo Lim <sloth@kookmin.ac.kr>
2016-02-11 01:43:58 +0900

    mod Setting class   - add constructer   - mod desrialize function

--- src/01.HW/library/happyhog/Setting.cpp
+++ src/01.HW/library/happyhog/Setting.cpp
@@ -1,12 +1,18 @@
-#include "Setting.h"
-
 #include <Arduino.h>
 
+#include "Setting.h"
+
+
 Setting::Setting() {};
+Setting::Setting(String& filePath, String& fileName) {
+	this->filePath = filePath;
+	this->fileName = fileName;
+};
 
 String& Setting::getFilePath() {
 	return filePath;
 }
+
 String& Setting::getFileName() {
 	return fileName;
 }
--- src/01.HW/library/happyhog/Setting.h
+++ src/01.HW/library/happyhog/Setting.h
@@ -10,10 +10,11 @@
 class Setting {
 public:
 	Setting::Setting();
+	Setting::Setting(String& filePath, String& fileName);
 	virtual ~Setting();
-
+	
+	virtual int deserialize(String& json) = 0;
 	virtual String serialize() = 0;
-	virtual int deserialize() = 0;
 		
 	String& getFilePath();
 	String& getFileName();
