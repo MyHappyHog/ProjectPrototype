4b58cfb8877a31a6ce9d4bd59da8e87f3eda7b8c
JongHo Lim <sloth@kookmin.ac.kr>
2016-02-11 09:38:19 +0900

    add H3Dropbox class   - inherit DataStore class   - implement upload and download

---
+++ src/01.HW/library/happyhog/H3Dropbox.cpp
@@ -0,0 +1,93 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+
+#include "H3Dropbox.h"
+
+H3Dropbox::H3Dropbox(String _key) : key(_key) {
+	invaildKey = false;
+}
+H3Dropbox::~H3Dropbox() { }
+
+int H3Dropbox::download(Setting* data) {
+	// http request 설정
+	HTTPClient* http = new HTTPClient();
+	
+	// https download url 연결
+	http->begin(DOWNLOAD_URL, CONTENT_FINGER_PRINT);
+	
+	// Authorization 헤더에 Auth 키 입력
+	http->addHeader(HEADER_AUTORIZATION, "Bearer " + key);
+	
+	// DROPBOX API 헤더에 Argument 입력
+	String filePathName = data->getFilePath() + data->getFileName();
+	String downloadArg = DOWNLOAD_ARGUMENT_TEMPLATE;
+	downloadArg.replace(PATH_CONTENT_KEY, filePathName);
+	http->addHeader(HEADER_DROPBOX_API_ARG, downloadArg);
+
+	// request 요청
+	int httpCode = http->POST("");
+
+	// response가 200(OK)이 아니면 종료.
+	/// @TODO invaild key 일 때 처리하기. 즉 에러처리..
+	if(httpCode != 200) {
+		delete http;
+		return -1;
+	} 
+
+	// response 가져와서
+	// json 이외의 문자들 제거
+	String result = http->getString();
+	result = result.substring(result.indexOf("{"), result.lastIndexOf("}") + 1);
+	
+	// httpclient 메모리 반환
+	delete http;
+
+	if (data->deserialize(result) != 0) {
+		return -1;
+	}
+
+	return 0;
+}
+
+int H3Dropbox::upload(Setting* data) {
+	// http request 설정
+	HTTPClient* http = new HTTPClient();
+	
+	// https upload url 연결
+	http->begin(UPLOAD_URL, CONTENT_FINGER_PRINT);
+	
+	// Authorization 헤더에 Auth 키 입력
+	http->addHeader(HEADER_AUTORIZATION, "Bearer " + key);
+	
+	// DROPBOX API 헤더에 Argument 입력
+	String filePathName = data->getFilePath() + data->getFileName();
+	String uploadArg = UPLOAD_ARGUMENT_TEMPLATE;
+	uploadArg.replace(PATH_CONTENT_KEY, filePathName);
+	uploadArg.replace(REV_CONTENT_KEY, "3e4404ddb9"); /// \TODO rev동적으로 변경
+	http->addHeader(HEADER_DROPBOX_API_ARG, uploadArg); 
+	
+	// Content-Type 헤더 추가
+	http->addHeader(HEADER_CONTENT_TYPE, UPLOAD_TYPE_VALUE);
+
+	// request 요청
+	int httpCode = http->POST(data->serialize());
+	
+	// response가 200(OK)이 아니면 종료.
+	/// @TODO invaild key 일 때 처리하기. 즉 에러처리..
+	if(httpCode != 200) {
+		delete http;
+		return -1;
+	} 
+
+	// response 가져와서
+	// json 이외의 문자들 제거
+	String result = http->getString();
+	result = result.substring(result.indexOf("{"), result.lastIndexOf("}") + 1);
+	
+	// httpclient 메모리 반환
+	delete http;
+
+	/// @TODO rev 확인해서 저장하기 추가.
+
+	return 0;
+}
---
+++ src/01.HW/library/happyhog/H3Dropbox.h
@@ -0,0 +1,41 @@
+#ifndef __H3DROPBOX_H__
+#define __H3DROPBOX_H__
+
+#include "DataStore.h"
+
+#define DOWNLOAD_URL "https://content.dropboxapi.com/2/files/download"
+#define UPLOAD_URL "https://content.dropboxapi.com/2/files/upload"
+#define CONTENT_FINGER_PRINT "E3 7F B0 09 DE E0 4E AB 3D 9D 44 F1 EC 38 64 C0 2B 85 90 12"
+
+#define HEADER_AUTORIZATION "Authorization"
+#define HEADER_CONTENT_TYPE "Content-Type"
+#define UPLOAD_TYPE_VALUE "application/octet-stream"
+
+#define HEADER_DROPBOX_API_ARG "Dropbox-API-Arg"
+
+#define PATH_CONTENT_KEY "<filePathContent>"
+#define REV_CONTENT_KEY "<revContent>"
+
+#define DOWNLOAD_ARGUMENT_TEMPLATE "{\"path\": \"<filePathContent>\"}"
+#define UPLOAD_ARGUMENT_TEMPLATE "{\"path\": \"<filePathContent>\", \"mode\":{\".tag\":\"update\",\"update\":\"<revContent>\"}}"
+
+/// @brief		HTTPS를 이용해 DROPBOX에 데이터를 저장하거나 가져오는 클래스
+/// @details	
+/// @author		Jongho Lim, sloth@kookmin.ac.kr
+/// @date		2016-02-11
+/// @version	0.0.1
+
+class H3Dropbox : public DataStore {
+public:
+	H3Dropbox(String _key);
+	virtual ~H3Dropbox();
+
+	virtual int download(Setting* data);
+	virtual int upload(Setting* data);
+	
+private:
+	bool invaildKey;
+	String key;
+};
+
+#endif
